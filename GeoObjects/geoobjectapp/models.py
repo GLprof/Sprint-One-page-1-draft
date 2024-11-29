ba252c0ec3f73b7d28bf76f516e1218b2199a130
GLprof <167696239+GLprof@users.noreply.github.com>
2024-11-29 13:22:40 +0300

    Update models.py

--- GeoObjects/geoobjectapp/models.py
+++ GeoObjects/geoobjectapp/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 class GeogrObject(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'New'),
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('rejected', 'Rejected'),
+    ]
+    
     title_object = models.CharField(max_length=128)
     latitude_object = models.FloatField()
     longitude_object = models.FloatField()
@@ -10,6 +17,8 @@ class GeogrObject(models.Model):
     user_name = models.CharField(max_length=128, unique=True)
     user_email = models.EmailField(max_length=256,)
     user_phone = models.CharField(max_length=128)
+
+    status = models.CharField(max_length=16, default='new', choices=STATUS_CHOICES)
 
     def __str__(self):
         return self.title_object
